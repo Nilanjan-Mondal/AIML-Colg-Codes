575164a875a6cd065f5b8fc970f03e80bf0f85e9
Nilanjan Mondal <nilanjanmondal@Nilanjans-MacBook-Air.local>
2025-01-16 12:51:34 +0530

    Added few panda codes

--- Pandas-Series-DataFrames.py
+++ Pandas-Series-DataFrames.py
@@ -7,11 +7,22 @@ dict = {
         'key2': 2
     }
 
-dtf = pd.Series(dict)
-temp = pd.DataFrame([dict])
+dtf1 = pd.Series(dict) 
+print(dtf1)
 
-print(dtf)
+
+# temp = pd.DataFrame(dict) 
+
+# If you pass dict directly to pd.DataFrame(dict), 
+# Pandas interprets each key as a column name and expects the values for each key to be iterable 
+#(like a list, tuple, or array). But in this case, the values (1 and 2) are integers, not iterables, 
+#so you'll get an error.
+
+
+temp = pd.DataFrame([dict]) 
 print(temp)
+
+# To create a DataFrame where the dictionary represents a row, wrap the dictionary in a list ([dict])
 
 
 
@@ -21,6 +32,26 @@ dict1 = {
     
     }
 
-dtf = pd.DataFrame(dict1)
+dtf2 = pd.DataFrame(dict1)
+print(dtf2)
 
-print(dtf)
+#In this case, the dictionary values are lists (iterables). Pandas automatically interprets this as:
+#Passing dict1 directly to pd.DataFrame(dict1) works because Pandas can map the lists as columns of the DataFrame:
+
+    
+
+dtf2_len = len(dtf2["name"]) # prints the size of 'name' of our data frame
+print("Count using len: ", dtf2_len)
+
+
+count = 0
+for n in dict1["name"]:   # manually prints the size of name in the dictionary
+    count += 1
+print("Count manual: ", count)
+
+
+
+# custom indexing in dataframe
+
+customIdxDtf2 = pd.DataFrame(dict1, index=['stu1', 'stu2', 'stu3'])
+print(customIdxDtf2)
---
+++ readingCsvUsingPandas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# reading a csv file using pandas
+
+path = "/Users/nilanjanmondal/Documents/AIML-Colg-Codes/utils/"
+
+data = pd.read_csv(path + "employees-1.csv")
+
+#print(data)
+
+
+# add one extra column by calculating from one existing column
+
+data1 = data
+
+data1 = data1.assign(commission_yearly = data1['salary'] * 0.2) # 20 percent commission
+
+print(data1)
