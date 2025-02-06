600e6f0e4cf3e81563c7353b00c9b5bc6e354f00
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-02-06 11:58:19 +0530

    Added more matplotlib plots

--- basicMatplotlibDemo.py
+++ basicMatplotlibDemo.py
@@ -7,6 +7,10 @@ yPoints = np.array([0, 250])
 
 plt.plot(xPoints, yPoints, marker = 'o')
 
+# plt.plot(xPoints, yPoints,'o') # Sample Plot with points only
+
+# plt.plot(xPoints, yPoints, linestyle = 'dotted', marker = 'o') # Sample Plot with points and dotted line
+
 plt.title("Sample Plot")
 
 plt.xlabel("X Axis")
